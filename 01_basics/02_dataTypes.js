672c466384b70625f35b9889e1cc5746ec8778f4
Ankita Adhikrao Chavan <er.ankitachavan@gmail.com>
2024-02-25 23:59:59 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone vale
+// undefined => not yet defined
+//symbol => unique
+
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+let accountNumber = 6789
+
+console.log(typeof accountNumber)
