dd484e21f7565832ceb29bef8eecfa310eeb87ca
Ankita Adhikrao Chavan <er.ankitachavan@gmail.com>
2024-02-25 23:30:24 +0530

    variables let const var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "anki@gmail.com"
+var accountNumber = 890
+accountName = "Ankita Chavan"
+
+
+accountEmail="try@gmail.com"
+accountNumber=676
+accountName = "Samrudhi Dube"
+
+console.log(accountId)
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountNumber,accountName])
