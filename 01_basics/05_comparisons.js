fac22800d0bb48945c32ef33f1c33a79c853fb78
Ankita Adhikrao Chavan <er.ankitachavan@gmail.com>
2024-03-03 23:53:24 +0530

    comparisons

---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,15 @@
+//console.log("2">1)
+
+//converts the data type from string to int
+//console.log("1"==1)
+
+//strictly cheks the data type and if data types are different returns false
+//console.log("1"===1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null<=0)
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
