7cae90b9abdd42952d3e8efca7b877cc4c783a41
Ankita Adhikrao Chavan <er.ankitachavan@gmail.com>
2024-03-03 23:26:11 +0530

    math operations

---
+++ 01_basics/04_mathOperations.js
@@ -0,0 +1,20 @@
+let variable = 3
+console.log(-variable)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+
+//prefix
+
+let var1 = 3
+let var2 = var1++
+let var3 = var1
+console.log(var1,var2,var3)
+
+let num1 = 3
+let num2 = ++num1
+console.log(num1,num2)
